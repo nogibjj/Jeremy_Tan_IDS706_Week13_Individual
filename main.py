a27884f27e5d0718c4b7deb18a7ba2a315b7be70
Jeremy Tan <36715338+jeremymtan@users.noreply.github.com>
2023-11-30 03:48:53 -0500

    finished and deployed

--- main.py
+++ main.py
@@ -1,19 +1,96 @@
-"""
-Main cli or app entry point
-"""
+from dotenv import load_dotenv
+import os
+import requests
+import json
+import base64
 
-from mylib.calculator import add
-import click
+load_dotenv()
 
-#var=1;var=2
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    request,
+    url_for,
+    jsonify,
+)
 
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+app = Flask(__name__)
+API_TOKEN = os.getenv("API_TOKEN")
+
+HEADERS = {"Authorization": f"Bearer {API_TOKEN}"}
+API_URL = "https://api-inference.huggingface.co/models/dandelin/vilt-b32-finetuned-vqa"
+
+
+@app.route("/")
+def index():
+    """retruns index page"""
+    return render_template("index.html")
+
+
+@app.route("/not_study", methods=["GET"])
+def not_study():
+    """returns not study html template"""
+    return render_template("not_study.html")
+
+
+@app.route("/study", methods=["GET"])
+def study():
+    """returns study html template"""
+    return render_template("study.html")
+
+
+def query(filename):
+    """query hugging face inferene api"""
+    with open(filename, "rb") as f:
+        data = f.read()
+
+    encoded_data = base64.b64encode(data).decode("utf-8")
+
+    payload = {
+        "inputs": {
+            "question": "is this person studying?",
+            "image": encoded_data,
+        }
+    }
+
+    response = requests.post(API_URL, headers=HEADERS, json=payload)
+    return json.loads(response.content.decode("utf-8"))
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    """test predict"""
+    file = request.files.get("image")
+    print(request.form)
+    use_default_image = request.form.get("use_default_image")
+    print(use_default_image)
+    if use_default_image != "false":
+        # Load the default image
+        result = query("static/final.jpg")
+    else:
+        if file and file.filename != "":
+            print(file)
+            # Save the uploaded file temporarily
+            temp_filename = "temp.jpg"
+            file.save(temp_filename)
+
+            # Perform the query using the uploaded file
+            result = query(temp_filename)
+
+            # Delete the temporary file
+            os.remove(temp_filename)
+        else:
+            return jsonify({"error": "No selected file"}), 400
+    yes_answer_score = [item["score"] for item in result if item["answer"] == "yes"]
+    no_answer_score = [item["score"] for item in result if item["answer"] == "no"]
+    yes_answer_score = yes_answer_score[0] if yes_answer_score else 0
+    no_answer_score = no_answer_score[0] if no_answer_score else 0
+    # grab results and check
+    if no_answer_score > yes_answer_score:
+        return redirect(url_for("not_study"))
+    return redirect(url_for("study"))
 
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    app.run(debug=True)
--- test_main.py
+++ test_main.py
@@ -2,9 +2,34 @@
 Test goes here
 
 """
+import os
+from dotenv import load_dotenv
+import requests
 
-from mylib.calculator import add
+
+load_dotenv()
+API_TOKEN = os.getenv("API_TOKEN")
+
+HEADERS = {"Authorization": f"Bearer {API_TOKEN}"}
+API_URL = "https://api-inference.huggingface.co/models/bert-base-uncased"
 
 
-def test_add():
-    assert add(1, 2) == 3
+def test_html_files(directory="templates/"):
+    """checks html files exists"""
+    html_files = [f for f in os.listdir(directory) if f.endswith(".html")]
+
+    for html_file in html_files:
+        file_path = os.path.join(directory, html_file)
+        assert os.path.exists(file_path) and os.path.isfile(file_path)
+
+
+def test_api():
+    """checks hugging api token works"""
+    test_payload = {"inputs": "The answer to the universe is [MASK]."}
+    response = requests.post(API_URL, headers=HEADERS, json=test_payload)
+    assert response.status_code == 200
+
+
+if __name__ == "__main__":
+    test_html_files()
+    test_api()
